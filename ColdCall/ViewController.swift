0b210fea87591db1e96511ace1ec8a8d08c8ee00
baalhumaidi <93419731+baalhumaidi@users.noreply.github.com>
2021-12-08 08:44:08 +0000

    -

--- ColdCall/ViewController.swift
+++ ColdCall/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var namelabel: UILabel!
+    @IBAction func Coldcall(_ sender: Any) {
+        
+     //  let randomnum = Int.random(in: 1...5)
+         let names = [ "Jay", "Nathan", "Michecl", "Ahmed", "Antony","Brian","Cody","Tom","Daniel","Frank","Suliman","Mohamed","Jimmy"]
+        let randomname = names.randomElement()
+        
+        namelabel.text = randomname
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
